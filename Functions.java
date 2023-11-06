dcee2861c266067b45b1cadb8f8aec8f76382ea2
Strelnikova <lizka.givka@mail.ru>
2023-11-06 14:52:33 +0300

    Added function of checking coordinates

--- Functions.java
+++ Functions.java
@@ -1,3 +1,5 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Functions {
 	public static void hello() {
 		System.out.println("Добро пожаловать в игру Морской бой!");
@@ -5,5 +7,10 @@ public class Functions {
 		"Каждый игрок может поставить 4 однопалубных корабля, 3 двухпалубных, 2 трехпалубных и 1 четырёхпалубный.\n" +
 		"Корабли можно располагать только по горизонтали или по вертикали.\n" +
 		"игроки не видят расположение кораблей друг друга.");
+	}
+	public static boolean rightCoordinate(String s) {
+		Pattern p = Pattern.compile("[A-Z]([1-9]|10)([ ][A-Z]([1-9]|10)){0,3}");
+		Matcher m = p.matcher(s);
+		return m.matches();
 	}
 }
